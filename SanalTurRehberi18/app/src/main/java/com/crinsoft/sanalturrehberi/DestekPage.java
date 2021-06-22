00b58f0959768336b38aea0b0ff904017ec3fd07
tripfinderr <trip.finderr@gmail.com>
2021-06-23 01:26:44 +0300

    TripFinder Android Studio

---
+++ SanalTurRehberi18/app/src/main/java/com/crinsoft/sanalturrehberi/DestekPage.java
@@ -0,0 +1,14 @@
+package com.crinsoft.sanalturrehberi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DestekPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destek_page);
+    }
+}
